b2c65589c23e0d403caafeb1c286bb1c0ca85d2d
richard.wu <richard.wu@ef.com>
2014-12-09 11:04:54 +0800

    Initial commit

---
+++ src/com/ef/core/Main.java
@@ -0,0 +1,10 @@
+package com.ef.core;
+
+/**
+ * Created by richard.wu on 12/9/2014.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello-world project");
+    }
+}
